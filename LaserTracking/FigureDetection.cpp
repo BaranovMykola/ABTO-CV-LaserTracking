3d3d61ff43d208fe6ea224ee0f8a820b82a30ffe
BaranovMykola <mapsg32@gmail.com>
2017-08-20 17:45:11 +0300

    Refactored figure detection

--- LaserTracking/FigureDetection.cpp
+++ LaserTracking/FigureDetection.cpp
@@ -19,12 +19,16 @@ using namespace std;
 using namespace cv;
 using namespace cv::ml;
 
-static int morphSize = 20;
+static int morphSize = 40;
 static int eps = 60;
 static int acc = 7;
 static int Rk = 1;
 static int elispeEps = 10;
 static std::string figuresName[7] = { "Triangle", "Rectangle", "Pentagon", "Hexagon", "Heptagon", "Octagon", "Nonagon" };
+static Scalar figureColor(0, 255, 0);
+static Scalar ellipseColor(255, 0, 0);
+static Scalar circleColor(255, 255, 0);
+static Scalar textColor(255, 255, 255);
 
 std::string detectFigure(cv::Mat & mask)
 {
@@ -41,7 +45,6 @@ std::string detectFigure(cv::Mat & mask)
 	do
 	{
 		Mat morph;
-		//dilate(maskGray, morph, getStructuringElement(MORPH_ELLIPSE, Size(morphSize, morphSize)));
 		morphologyEx(maskGray, morph, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, Size(morphSize, morphSize)));
 
 		vector < vector<Point> > contours;
@@ -50,35 +53,6 @@ std::string detectFigure(cv::Mat & mask)
 		drawContours(morph, contours, -1, Scalar(255, 0, 0));
 		imshow("morph", morph);
 		Mat draw = mask.clone();
-
-		/*for (auto i : contours)
-		{
-			Rect area = boundingRect(i);
-			eps = std::min(area.height, area.width) / 3;
-			acc = eps / 10;
-
-			vector<Point> approx;
-			vector<Point> accuracyContour;
-			approxPolyDP(i, approx, eps, true);
-			approxPolyDP(i, accuracyContour, acc, true);
-
-			drawContours(draw, vector<vector<Point>>{approx}, -1, Scalar(0, 255, 0), 1);
-			drawContours(draw, vector<vector<Point>>{accuracyContour}, -1, Scalar(255, 255, 255), 1);
-
-			//cout << "Accuracy contour corners - " << accuracyContour.size() << endl;
-			if (isCircle(i, approx, accuracyContour, draw, figure))
-			{		
-					cout << "Found circle " << endl;
-					figure = figuresName[4];
-			}
-			else
-			{
-				figure = figuresName[approx.size() - 3];
-			}
-			cout << "Founded " << approx.size() << " corners " << endl;
-		}
-		putText(draw, figure, Point(100, 100), FONT_HERSHEY_COMPLEX, 2, Scalar::all(255));*/
-
 		vector<int> childrensIndex;
 
 		for (int i = 0; i < contours.size(); i++)
@@ -89,13 +63,12 @@ std::string detectFigure(cv::Mat & mask)
 			auto it = std::find(childrensIndex.begin(), childrensIndex.end(), i);
 			if (!children.empty())
 			{
-			//	drawContours(draw, children, -1, Scalar(255,255,255), 1);
 				for (auto j : children)
 				{
 					checkFigure(j, draw);
 				}
 			}
-			else if(it == childrensIndex.end())
+			else if (it == childrensIndex.end())
 			{
 				drawContours(draw, contours, i, Scalar(255, 0, 0), 1);
 				float lenght = cv::arcLength(contours[i], true);
@@ -105,45 +78,16 @@ std::string detectFigure(cv::Mat & mask)
 				str >> l;
 
 				Point center = contours[i][0];
-							center.y += 15;
 				putText(draw, l, center, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 1, Scalar::all(255));
 			}
 		}
-
 		imshow("figure", draw);
-	} while (waitKey(30) != 27);
+	}
+	while (waitKey(30) != 27);
 	destroyWindow("figure");
 	destroyWindow("morph");
 	destroyWindow("Panel");
 	return figure;
-}
-
-bool isCurve(vector<Point> & points)
-{
-	cout << "area square = " << contourArea(points) << endl;
-	return contourArea(points) > 0;
-}
-
-bool isCircle(std::vector<Point> i, std::vector<Point> approx, std::vector<Point> accuracyContour, cv::Mat& draw, std::string& figure)
-{
-	Point2f center;
-	float radius;
-	minEnclosingCircle(i, center, radius);
-	//RotatedRect elipse = fitEllipse(i);
-
-	cout << "R = " << radius << endl;
-	int acc = accuracyContour.size();
-	int apr = approx.size();
-	float lenghtCircle = 2 * CV_PI*radius;
-	float lenghtFigure = arcLength(i, true);
-	float lenghtRatio = lenghtFigure / lenghtCircle;
-	float squareFigure = contourArea(i);
-	float squareCircle = CV_PI*radius*radius;
-	float squareRatio = squareFigure / squareCircle;
-
-	cout << "FigureS / Circle S = " << squareFigure << "/" << squareCircle << " = " << squareRatio << endl;
-	circle(draw, center, radius, Scalar(255, 0, 0), 1);
-	return (squareRatio > 0.7 || (apr <= 2 || apr > 6));
 }
 
 std::vector<std::vector<cv::Point>> getChildren(std::vector<std::vector<cv::Point>> contours, std::vector<cv::Vec4i> hierarchy, int item, vector<int>& childrensIndex)
@@ -161,77 +105,60 @@ std::vector<std::vector<cv::Point>> getChildren(std::vector<std::vector<cv::Poin
 
 void checkFigure(std::vector<cv::Point> contour, cv::Mat & draw)
 {
+	const float circleRatioThresh = 0.65;
+	const float ellipseRatiothresh = 0.95;
+
 	Rect area = boundingRect(contour);
 	int aprEps = std::min(area.width, area.height)*0.2;
 	int accEps = aprEps*0.3;
-	string name;
 
+	string name;
 	vector<Point> approx;
 	vector<Point> accuaracy;
 
 	approxPolyDP(contour, approx, aprEps, true);
 	approxPolyDP(contour, accuaracy, accEps, true);
 
-	/*drawContours(draw, std::vector<vector<Point>> { approx }, -1, Scalar(0, 255, 0), 2);
-	drawContours(draw, std::vector<vector<Point>> { accuaracy }, -1, Scalar(255, 255, 255), 2);*/
-
 	Point2f center;
 	float radius;
 	minEnclosingCircle(contour, center, radius);
 	auto rect = fitEllipse(contour);
-	
+
 	float circleS = CV_PI*radius*radius;
 	float figureS = contourArea(contour);
-	float ratioS = figureS/circleS;
+	float ratioS = figureS / circleS;
 	float ratioE = figureS / ellipseSquare(rect);
 
 	cout << "figureS/circleS = " << figureS << "/" << circleS << " = " << ratioS << endl;
 	cout << "figureS/ellipseS = " << figureS << "/" << ellipseSquare(rect) << " = " << ratioE << endl;
 	cout << endl;
 
-	name = figuresName[approx.size()-3];
+	name = figuresName[approx.size() - 3];
 
 	int acc = accuaracy.size();
 	int accThresh = 7;
 
 	bool smooth = acc > accThresh;
 
-/*	if (ratioS > 0.65 && ratioE > 0.95 && smooth)
+	if (ratioS > circleRatioThresh && smooth)
 	{
-		if (ratioS > 0.65)
-		{
-			circle(draw, center, radius, Scalar(255, 40, 100), 2);
-			name = "Circle";
-		}
-		else
-		{
-			ellipse(draw, rect, Scalar(255, 40, 100), 2);
-			name = "Elipse";
-		}
-	}
-
-	else */if(ratioS > 0.65 && smooth)
-	{
-		circle(draw, center, radius, Scalar(255, 40, 100), 2);
+		circle(draw, center, radius, circleColor, 2);
 		name = "Circle";
 	}
-	else if (ratioE > 0.95 && smooth)
+	else if (ratioE > ellipseRatiothresh && smooth)
 	{
-		ellipse(draw, rect, Scalar(255, 40, 100), 2);
+		ellipse(draw, rect, ellipseColor, 2);
 		name = "Elipse";
 	}
 	else
 	{
-
-		drawContours(draw, std::vector<vector<Point>> { approx }, -1, Scalar(0, 255, 0), 2);
+		drawContours(draw, std::vector<vector<Point>> { approx }, -1, figureColor, 2);
 	}
 
-
-	Point t = contour[0];
-	putText(draw, name, t, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 1, Scalar::all(255), 1);
+	putText(draw, name, contour[0], cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 1, textColor, 1);
 }
 
 float ellipseSquare(cv::RotatedRect & el)
 {
-	return CV_PI*el.size.height*el.size.width/4;
+	return CV_PI*el.size.height*el.size.width / 4;
 }
--- LaserTracking/FigureDetection.h
+++ LaserTracking/FigureDetection.h
@@ -5,10 +5,6 @@
 
 std::string detectFigure(cv::Mat& mask);
 
-bool isCurve(std::vector<cv::Point>& points);
-
-bool isCircle(std::vector<cv::Point> i, std::vector<cv::Point> approx, std::vector<cv::Point> accuracyContour, cv::Mat & draw, std::string & figure);
-
 std::vector<std::vector<cv::Point>> getChildren(std::vector<std::vector<cv::Point>> contours, std::vector<cv::Vec4i> hierarchy, int item, std::vector<int>& childrensIndex);
 
 void checkFigure(std::vector<cv::Point> contour, cv::Mat& draw);
